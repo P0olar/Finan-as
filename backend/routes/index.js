a9a9784fd4db8c9a2724001e6366741f9c7a7976
P0olar <santosvictorfernando20@gmail.com>
2025-11-25 19:49:31 -0300

    feat: updade das drist

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
 import express from "express";
+import path from "path";
 import { Router } from "express";
 import UserRoutes from "../domain/users/routes.js";
 import ReportRoutes from "../domain/report/routes.js";
-import path from "path";
 
 const __dirname = path.resolve();
 
@@ -10,9 +10,9 @@ const router = Router();
 
 router.use("/users", UserRoutes);
 router.use("/report", ReportRoutes);
-router.use(express.static(path.join(__dirname, `../../frontend/dist`)));
+router.use(express.static(path.join(__dirname, `../frontend/dist`)));
 router.get("*", async (request, response) => {
-  response.sendFile(path.join(__dirname, `../../frontend/dist/index.html`));
+  response.sendFile(path.join(__dirname, `../frontend/dist/index.html`));
 });
 
 export default router;
