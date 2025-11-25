5925e274fcb553301ae4dea39334f140ba41100f
P0olar <santosvictorfernando20@gmail.com>
2025-11-25 20:08:14 -0300

    feat: update com dados on

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,18 +1,10 @@
-import express from "express";
-import path from "path";
 import { Router } from "express";
 import UserRoutes from "../domain/users/routes.js";
 import ReportRoutes from "../domain/report/routes.js";
-
-const __dirname = path.resolve();
 
 const router = Router();
 
 router.use("/users", UserRoutes);
 router.use("/report", ReportRoutes);
-router.use(express.static(path.join(__dirname, `../frontend/dist`)));
-router.get("*", async (request, response) => {
-  response.sendFile(path.join(__dirname, `../frontend/dist/index.html`));
-});
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,20 @@ import express from "express";
 import cors from "cors";
 import routers from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ORIGIN_CORS = process.env.ORIGIN_CORS || "http://localhost:5173";
 
 export const app = express();
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -15,4 +24,5 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(routers);
