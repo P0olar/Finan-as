ecaa18b006000c07495693fca8225e008cf20880
P0olar <santosvictorfernando20@gmail.com>
2025-11-25 19:42:32 -0300

    feat: updade backend e frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,18 @@
+import express from "express";
 import { Router } from "express";
-import UserRoutes from "../domain/users/routes.js"
-import ReportRoutes from "../domain/report/routes.js"
+import UserRoutes from "../domain/users/routes.js";
+import ReportRoutes from "../domain/report/routes.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const router = Router();
 
 router.use("/users", UserRoutes);
 router.use("/report", ReportRoutes);
-
+router.use(express.static(path.join(__dirname, `../../frontend/dist`)));
+router.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, `../../frontend/dist/index.html`));
+});
 
 export default router;
